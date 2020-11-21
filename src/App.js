8b13200f32616bea3017bea09c9e7c83f41c8ac0
Houston <HoustonEU@gmail.com>
2020-11-21 17:10:23 +0300

    App has been changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import React from "react";
+import {Route, Switch} from "react-router-dom";
+import NoMatch from "./components/NoMatch/NoMatch";
+import NewsListPage from "./components/NewsListPage/NewsListPage";
+import NewsPageContainer from "./components/NewsPage/NewsPageContainer";
+import Header from "./components/Header/Header";
 
 function App() {
     return (
-        <div>
-
-        </div>
+        <main className={styles.appContent}>
+            <Header/>
+            <Switch>
+                <Route path={"/"} exact render={() => <NewsListPage/>} />
+                <Route path={"/news:newsId"} render={() => <NewsPageContainer />} />
+                <Route render={() => <NoMatch />}/>
+            </Switch>
+        </main>
     );
 }
 
