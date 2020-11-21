b35c66b051999640e51866fe8da238e1d549a860
Houston <HoustonEU@gmail.com>
2020-11-21 17:12:31 +0300

    Title has been created

---
+++ src/components/NewsListPage/Title/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Title.module.css"
+
+const Title =  (props) => {
+    return (
+        <div className={styles.titleContent}>
+            Latest News
+        </div>
+    );
+};
+
+export default Title;
